99ae851e91f822296e8201b8993266c09aa7f42e
matthias haefeli <haefeli.matthias@gmail.com>
2020-08-23 11:46:25 -0600

    add user authentication

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,27 @@
+class UsersController < ApplicationController
+  def create
+    user = User.new(user_params)
+    if user.save
+      token = JsonWebToken.encode({userId: user.id})
+      render json: {token: token}.to_json
+    else
+      render json: user.errors, status: :bad_request
+    end
+  end
+
+  def signin
+      user = User.find_by(user_params)
+    if user
+      token = JsonWebToken.encode({userId: user.id})
+      render json: {token: token}.to_json
+    else
+      render json: {error: 'Invalid Request'}, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+end
